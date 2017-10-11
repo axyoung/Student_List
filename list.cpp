d324fcf0e25b83a3bcbf5478a6d681c5e95fa152
cixay <alex.shan.young@gmail.com>
2017-10-11 11:14:30 -0700

    done, but research iterators

--- list.cpp
+++ list.cpp
@@ -2,16 +2,10 @@
  * Student List
  * Alex Young
  * This project will allow you to read in, print out, and delete students
- * 10/3/2017
+ * 10/11/2017
  */
 
-/* Ok so the basic idea is that that there is a vector that will store stuct pointers
- * when you add a student to the list, you will push back and add one to the vector
- * these struct pointers will be accessed when you print out the list using a for loop and vector.at(index)
- * then erase the vector at a position to delete
- * To quit just return, otherwise repeat a loop
- * check out programminghelporg tutorials
- */
+// It is done, but need get it to use iterators instead for printing and erasing
 
 #include <iostream>
 #include <vector>
@@ -20,6 +14,7 @@
 
 using namespace std;
 
+// stuct that stores each student
 struct Student {
 	char first[21];
 	char last[21];
@@ -32,27 +27,35 @@ void print(const vector<Student>&);
 void erase(vector<Student>&);
 
 int main() {
+	// program runs until continue is false, which is when you QUIT
 	bool cont = true;
+	// vector that hold the students, this is my list
 	vector<Student> myVector;
 
+	// while the programs runs go through the loop and check for commands
 	while (cont) {
 		cout << "Student List, you can (ADD, PRINT, DELETE, or QUIT)" << endl;
+		// I will get their entry, which will be ADD, PRINT, DELETE, or QUIT, or else ask again
 		char entry[10] = "";
 		cin.get(entry, sizeof(entry));
 		cin.get();
 
+		// if they ask to add, run add function, passing in the list of students
 		if (strcmp(entry, "ADD") == 0) { 
 			add(myVector);
 		}
 
+		// if they say print, run print function, passing list of students
 		else if (strcmp(entry, "PRINT") == 0) { 
 			print(myVector);
 		}
 
+		// if they want to delete an entry, run corresponding function
 		else if (strcmp(entry, "DELETE") == 0) { 
 			erase(myVector);
 		}
 
+		// if they want to exit, stop the program
 		else if (strcmp(entry, "QUIT") == 0) { 
 			cont = false;
 		}
@@ -67,6 +70,7 @@ void add(vector<Student>& myVector) {
 	// empty local variable for student struct
 	Student s = { 0 };
 
+	// get what they enter for names, id and gpa, then set the local struct to that value
 	cout << "Student First Name: ";
 	cin.get(s.first, sizeof(s.first));
 	cin.get();
@@ -89,14 +93,25 @@ void add(vector<Student>& myVector) {
 
 // function to print out all the students stored
 void print(const vector<Student>& myVector) {
+	// use an unsigned int because it is a vector
 	for (unsigned int i = 0; i < myVector.size(); i++) {
+		// use fixed and set precision for 2 decimal places for gpa
 		cout << myVector[i].first << " " << myVector[i].last << ", " << myVector[i].id << ", "
 		     << fixed << setprecision(2) << myVector[i].gpa << endl;
 	}
 }
 
-// function to delete a student from the list
+// function to prompt for student id to delete a student from the list
 void erase(vector<Student>& myVector) {
+	int deleteID = 0;
+	cout << "Student ID to delete: ";
+	cin >> deleteID;
+	cin.get();
+	for (unsigned int i = 0; i < myVector.size(); i++) {
+		if (deleteID == myVector[i].id) {
+			myVector.erase(myVector.begin() + i);
+		}
+	}
 	// check out vector begin and vector erase
 	// need to use an iterator to erase
 	// just need to erase, not empty, because not using pointers
